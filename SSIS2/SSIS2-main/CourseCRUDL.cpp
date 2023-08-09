4a955aa5efe9498444f295797d2ceb009d991b47
gel <88552693+Airplanejuju@users.noreply.github.com>
2023-08-09 10:52:59 +0800

    Add files via upload

---
+++ SSIS2/SSIS2-main/CourseCRUDL.cpp
@@ -0,0 +1,2 @@
+#include "CourseCRUDL.h"
+
---
+++ SSIS2/SSIS2-main/MainForm.cpp
@@ -0,0 +1,12 @@
+#include "MainForm.h"
+
+using namespace ssisguitest1;
+
+[STAThreadAttribute]
+
+int main()
+{
+	Application::Run(gcnew MainForm());
+
+	return 0;
+}
